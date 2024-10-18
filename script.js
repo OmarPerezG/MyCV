a94d54fcb87889f91573dc8645efe5a61ed8b59f
Omarpg <85317666+OmarPerezG@users.noreply.github.com>
2024-10-18 11:12:19 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// Función para cambiar el idioma
+function toggleLanguage() {
+    // Aquí iría la lógica para cambiar el idioma entre español e inglés
+    alert("Cambiando idioma...");
+}
+
+// Función para mostrar la alerta
+function showAlert() {
+    document.getElementById('alert').style.display = 'flex';
+}
+
+// Función para cerrar la alerta
+function closeAlert() {
+    document.getElementById('alert').style.display = 'none';
+}
