8d0487eff45e814ca9fff924dffa47c07b888b4d
Omarpg <85317666+OmarPerezG@users.noreply.github.com>
2024-10-21 15:10:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-// Función para cambiar el idioma
-function toggleLanguage() {
-    alert("Cambiando idioma...");
-}
-
-// Función para mostrar la alerta de información de proyectos
 function showAlert() {
-    document.getElementById('alert').style.display = 'flex';
+    document.getElementById("alert").style.display = "flex";
 }
 
-// Función para cerrar la alerta
 function closeAlert() {
-    document.getElementById('alert').style.display = 'none';
+    document.getElementById("alert").style.display = "none";
+}
+
+function toggleLanguage() {
+    const currentLang = document.querySelector(".language-toggle").textContent;
+    if (currentLang === "ES") {
+        document.querySelector(".language-toggle").textContent = "EN";
+        document.querySelector("h1").textContent = "Omar Pérez Gutiérrez";
+        document.querySelector("p").textContent = "Software Development and Management Engineer.";
+        document.querySelector("#acerca-de-mi p").textContent = "I am a passionate software developer with experience in creating innovative and efficient technological solutions.";
+        document.querySelector("#educacion h2").textContent = "Education";
+        document.querySelector("#educacion ul li").innerHTML = "<strong>Technological University of Manzanillo</strong> - Bachelor's Degree in Software Development and Management";
+        document.querySelector("#experiencia h2").textContent = "Work Experience";
+        document.querySelector("#experiencia ul li strong").textContent = "Nexon Automation - Controls Engineer";
+        document.querySelector("#hobbies h2").textContent = "Hobbies";
+    } else {
+        document.querySelector(".language-toggle").textContent = "ES";
+        document.querySelector("h1").textContent = "Omar Pérez Gutiérrez";
+        document.querySelector("p").textContent = "Ing. en Desarrollo y Gestión de Software.";
+        document.querySelector("#acerca-de-mi p").textContent = "Soy un apasionado desarrollador de software con experiencia en la creación de soluciones tecnológicas innovadoras y eficientes.";
+        document.querySelector("#educacion h2").textContent = "Educación";
+        document.querySelector("#educacion ul li").innerHTML = "<strong>Universidad Tecnológica de Manzanillo</strong> - Licenciatura en Desarrollo y Gestión de Software";
+        document.querySelector("#experiencia h2").textContent = "Experiencia Laboral";
+        document.querySelector("#experiencia ul li strong").textContent = "Nexon Automation - Ingeniero de Controles";
+        document.querySelector("#hobbies h2").textContent = "Hobbies";
+    }
 }
